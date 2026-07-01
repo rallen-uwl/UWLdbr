32b36f4534d4aaf641b8bb34ed2015799cd9bc39
rallen-uwl <rallen@uwlax.edu>
2026-07-01 10:21:16 -0500

    commit

--- R/interface.R
+++ R/interface.R
@@ -8,7 +8,7 @@ db_path <- "https://raw.githubusercontent.com/rallen-uwl/uwldb/main/"
 #' @return A dataframe containing program name database.
 #' @export
 get_program_name_db <- function() {
-  readr::read_csv(paste0(db_path, "program_name_db.csv"))  |>
+  readr::read_csv(paste0(db_path, "program_names_db.csv"))  |>
     dplyr::select(-dplyr::all_of(tidyselect::starts_with("..."))) |>
     dplyr::rename_with(~stringr::str_replace_all(., " ", ".")) |>
     dplyr::select_all(tolower)
